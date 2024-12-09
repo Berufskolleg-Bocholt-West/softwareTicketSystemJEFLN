8abf308d65ee2369ab843a8b7dc2ae5b9e3daa30
Inamoas <141828158+Inamoas@users.noreply.github.com>
2024-12-09 09:14:08 +0100

    Add files via upload

---
+++ Ticketsystem/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module de.bkbocholt.ticketsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens de.bkbocholt.ticketsystem to javafx.fxml;
+    exports de.bkbocholt.ticketsystem;
+}
