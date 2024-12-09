73d5ebf3da64957db0da5f184013bfe749d21064
Inamoas <141828158+Inamoas@users.noreply.github.com>
2024-12-09 09:21:01 +0100

    Delete Ticketsystem directory

--- Ticketsystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.bkbocholt.ticketsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens de.bkbocholt.ticketsystem to javafx.fxml;
-    exports de.bkbocholt.ticketsystem;
-}
