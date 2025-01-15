cbbe3b995b164fbc29d4f13729f66d02c7e9b778
Inamoas <slein.1607@gmail.com>
2025-01-15 12:49:37 +0100

    Basic Methods

---
+++ Ticketsystem/src/main/java/de/bkbocholt/model/Administrator.java
@@ -0,0 +1,8 @@
+package de.bkbocholt.model;
+
+public class Administrator extends User {
+    public Administrator(int userId, String firstName, String lastName, String email, String password) {
+        super(userId, firstName, lastName, email, password);
+    }
+
+}
