f5e1a4a8ecd5ea8f34ea84c7a4eb890bf6482143
Inamoas <slein.1607@gmail.com>
2025-01-20 08:33:31 +0100

    Basic Methods

--- Ticketsystem/src/main/java/de/bkbocholt/model/User.java
+++ Ticketsystem/src/main/java/de/bkbocholt/model/User.java
@@ -19,26 +19,26 @@ public class User {
         this.assingedRoles = new ArrayList<>();
     }
 
-    public int getUserId() {
-        return userId;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public ArrayList<Role> getAssingedRoles() {
-        return assingedRoles;
+    public int getUserId() {return userId;}
+    public void setUserId() {this.userId = userId;}
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+    public ArrayList<Role> getAssingedRoles() {return assingedRoles;}
+    public void setAssingedRoles(ArrayList<Role> assingedRoles) {this.assingedRoles = assingedRoles;}
+
+    public void saveToFile() {
+
     }
 
+    public void readFromFile() {
 
+    }
 }
 
 
